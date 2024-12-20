c2c380cf2f7cd25df19fa50142aea6bcf15442b1
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2024-12-20 17:52:18 +0000

    commit

--- java/Hello.java
+++ java/Hello.java
@@ -3,5 +3,11 @@ class Hello
     public static void main(String args[])
     {
         System.out.println("Hello world");
+        Bello b = new Bello();
     }
+}
+
+class Bello extends Hello
+{
+    
 }
